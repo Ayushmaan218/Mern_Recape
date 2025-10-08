c2909499391a872f36242c179402991167745cdf
Ayushmaan218 <121386512+Ayushmaan218@users.noreply.github.com>
2025-10-08 19:17:42 +0530

    Completed Project

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import productRouts from "./routes/product.routes.js"
+import productRoutes from "./routes/product.routes.js"
+import path from "path";
+
+dotenv.config();
 
 const app = express();
-dotenv.config();
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use("/api/products", productRouts);
+const __dirname = path.resolve();
 
-//console.log(process.env.MONGO_URI)
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.listen(port, ()=>{
-    connectDB();;
-    console.log("the server started at http://localhost:"+port)
-})
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+});
 
